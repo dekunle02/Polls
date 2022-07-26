e8d97913cf87c27f68e3a6257f9adfdd79485f8a
dekunle02 <dekunle02@gmail.com>
2022-07-26 14:34:55 +0100

    chore:update secret key

--- Polls/Polls/settings.py
+++ Polls/Polls/settings.py
@@ -12,7 +12,7 @@ DEVELOPMENT_MODE = os.getenv("DEVELOPMENT_MODE", "False") == "True"
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
+SECRET_KEY = os.environ["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
